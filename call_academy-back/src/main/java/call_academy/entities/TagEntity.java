0687fd380fb2e0ee96b537206515a7f44e7daa50
juan62976 <jj.valencia@uniandes.edu.co>
2021-06-16 17:11:12 -0500

    Clase Tag

--- call_academy-back/src/main/java/call_academy/entities/TagEntity.java
+++ call_academy-back/src/main/java/call_academy/entities/TagEntity.java
@@ -5,11 +5,53 @@
  */
 package call_academy.entities;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
 /**
  *
  * @author juanj
  */
+@Entity
 public class TagEntity {
+    
+    private String id; 
+    private String nombre; 
+
+    
+    
+    
+   /**
+    * 
+    * @return otiene el id 
+    */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id set the id 
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 
+     * @return obtiene el nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * 
+     * @param nombre set the nombre 
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
     
     
